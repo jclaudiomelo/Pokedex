490f5f425a911cf8c90867b4ca100d639c0f3031
Claudio Melo <claudiojcmn@yahoo.com.br>
2023-09-30 22:43:44 -0300

    pokedex

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-function pokeapi(){
-debugger
-var busca = document.getElementById('busca').value;
-var url = `https://pokeapi.co/api/v2/pokemon/${busca}`
+function pokeapi() {
+ 
+  var busca = document.getElementById("busca").value;
+  var url = `https://pokeapi.co/api/v2/pokemon/${busca}`;
 
-fetch(url)
-    .then((resp) => resp.json())
-    document.getElementById('nome')
-    console.log()
+  fetch(url).then((resp) => resp.json());
+  document.getElementById("nome");
 }
 
-
 function pokeapi() {
-    var busca = document.getElementById('busca').value;
-    var url = `https://pokeapi.co/api/v2/pokemon/${busca}`;
-  
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => {
-          
-        document.getElementById('nome').textContent = `Name: ${data.name}`;
-        
-        console.log(data)
-        
-      })
-      .catch((error) => {
-        console.error(alert(error));
+  var busca = document.getElementById("busca").value;
+  var url = `https://pokeapi.co/api/v2/pokemon/${busca}`;
+
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      document.getElementById("nome").textContent = data.name;
+      document.getElementById("id").textContent = data.id;
+      var url_img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`;
+      document.getElementById("img_pokemon").src = url_img;
+
+    })
+    .catch((error) => {
+        alert(error)
       });
-  }
-  
+}
