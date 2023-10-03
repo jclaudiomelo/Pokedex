6443b241113741bc05a923e6ef54f55f02430af6
Claudio Melo <claudiojcmn@yahoo.com.br>
2023-10-03 16:08:51 -0300

    atualizado unica function

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
-function pokeapi(paramater) {
-  var busca = document.getElementById("busca").value;
-
-  var url = `https://pokeapi.co/api/v2/pokemon/${busca || id}`;
-
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-      document.getElementById("nome").textContent = data.name;
-      document.getElementById("id").textContent = data.id;
-      var url_img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`;
-      document.getElementById("img_pokemon").src = url_img;
-      id = document.getElementById("id").textContent = data.id;
-    })
-    .catch((error) => {
-      alert(error);
-    });
-}
-
-var id = 1;
-
-function NextOrPrev(paramater) {
+function buscarPokemon(paramater) {
+  if (paramater == "buscar") {
+    var id = document.getElementById("buscar").value;
+  }
   if (paramater == "next") {
+    var id = document.getElementById("id").textContent;
     id++;
   }
   if (paramater == "prev") {
+    var id = document.getElementById("id").textContent;
     id--;
   }
   if (paramater == "ligar") {
     id = 1;
   }
   if (id == 0) {
-    NextOrPrev("ligar");
-  } else {
+    pokeapi("ligar");
   }
 
   var url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -39,7 +22,6 @@ function NextOrPrev(paramater) {
   fetch(url)
     .then((resp) => resp.json())
     .then((data) => {
-      console.log(id);
       document.getElementById("nome").textContent = data.name;
       document.getElementById("id").textContent = data.id;
       var url_img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`;
