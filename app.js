a9125d567e3357fa927c9ed5015adc7300997d17
Claudio Melo <claudiojcmn@yahoo.com.br>
2023-10-02 21:03:04 -0300

    work NEXT, PREV, POWER = 1 E BUSCA char e id

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 function pokeapi(paramater) {
   var busca = document.getElementById("busca").value;
 
-  var url = `https://pokeapi.co/api/v2/pokemon/${busca}`;
-  console.log("busca", busca,id);
-  id = busca
+  var url = `https://pokeapi.co/api/v2/pokemon/${busca || id}`;
+
   fetch(url)
     .then((resp) => resp.json())
     .then((data) => {
@@ -11,42 +10,36 @@ function pokeapi(paramater) {
       document.getElementById("id").textContent = data.id;
       var url_img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`;
       document.getElementById("img_pokemon").src = url_img;
+      id = document.getElementById("id").textContent = data.id;
     })
     .catch((error) => {
       alert(error);
     });
 }
 
-
+var id = 1;
 
 function NextOrPrev(paramater) {
   if (paramater == "next") {
-    console.log("next", busca);
     id++;
   }
   if (paramater == "prev") {
     id--;
-  } 
-  if (paramater == "ligar") {
-    
-    id = 1;
-    
-  } if (id == 0) {
-    
-    NextOrPrev("ligar")
-    
   }
-  else {
-    
+  if (paramater == "ligar") {
+    id = 1;
+  }
+  if (id == 0) {
+    NextOrPrev("ligar");
+  } else {
   }
 
   var url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
- 
   fetch(url)
     .then((resp) => resp.json())
     .then((data) => {
-      console.log(id)
+      console.log(id);
       document.getElementById("nome").textContent = data.name;
       document.getElementById("id").textContent = data.id;
       var url_img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`;
@@ -56,16 +49,3 @@ function NextOrPrev(paramater) {
       alert(error);
     });
 }
-
-// function nextPoke() {
-//   idNext = 1;
-//   nextPoke = document
-//     .getElementById("nextButton")
-//     .addEventListener("click", function () {
-//       idNext++;
-
-//       document.getElementById("busca").value = idNext;
-//       pokeapi();
-//     });
-
-// }
